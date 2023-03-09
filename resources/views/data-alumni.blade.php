b921d5022841cf0369b419b66398f0e8b0232d07
Re-Creators <richardoehbet221@gmail.com>
2023-03-09 21:40:34 +0700

    add responsive page

--- resources/views/data-alumni.blade.php
+++ resources/views/data-alumni.blade.php
@@ -42,96 +42,28 @@
             <p class="text-center">Hasil pencarian untuk "<span class="fw-bold">Rudi</span>"</p>
         </div>
         <div class="row mt-1 gy-3">
-            <div class="col-md-3">
-                <div class="card text-center shadow-sm">
-                    <div class="card-header">
-                        <h5 class="fw-semibold">IPA 3</h5>
-                        <h6>2019</h6>
-                    </div>
-                    <div class="card-body">
-                        <div class="student-img">
-                            <img src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
-                                alt="" class="img-fluid">
+            @for ($i = 0; $i < 10; $i++)
+                <div class="col-md-6 col-lg-4">
+                    <div class="card text-center shadow-sm">
+                        <div class="card-header">
+                            <h5 class="fw-semibold">IPA 3</h5>
+                            <h6>2019</h6>
                         </div>
-                        <div class="mt-3">
-                            <h5 class="card-title fw-semibold">Abrahan GT Sinaga</h5>
-                            <p class="card-text">abramtambatuasinaga@gmail.com</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-3">
-                <div class="card text-center shadow-sm">
-                    <div class="card-header">
-                        <h5 class="fw-semibold">IPA 3</h5>
-                        <h6>2019</h6>
-                    </div>
-                    <div class="card-body">
-                        <div class="student-img">
-                            <img src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
-                                alt="" class="img-fluid">
-                        </div>
-                        <div class="mt-3">
-                            <h5 class="card-title fw-semibold">Abrahan GT Sinaga</h5>
-                            <p class="card-text">abramtambatuasinaga@gmail.com</p>
+                        <div class="card-body">
+                            <div class="student-img">
+                                <img src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+                                    alt="" class="img-fluid">
+                            </div>
+                            <div class="mt-3">
+                                <h5 class="card-title fw-semibold">Abrahan GT Sinaga</h5>
+                                <p class="card-text">abramtambatuasinaga@gmail.com</p>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-md-3">
-                <div class="card text-center shadow-sm">
-                    <div class="card-header">
-                        <h5 class="fw-semibold">IPA 3</h5>
-                        <h6>2019</h6>
-                    </div>
-                    <div class="card-body">
-                        <div class="student-img">
-                            <img src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
-                                alt="" class="img-fluid">
-                        </div>
-                        <div class="mt-3">
-                            <h5 class="card-title fw-semibold">Abrahan GT Sinaga</h5>
-                            <p class="card-text">abramtambatuasinaga@gmail.com</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-3">
-                <div class="card text-center shadow-sm">
-                    <div class="card-header">
-                        <h5 class="fw-semibold">IPA 3</h5>
-                        <h6>2019</h6>
-                    </div>
-                    <div class="card-body">
-                        <div class="student-img">
-                            <img src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
-                                alt="" class="img-fluid">
-                        </div>
-                        <div class="mt-3">
-                            <h5 class="card-title fw-semibold">Abrahan GT Sinaga</h5>
-                            <p class="card-text">abramtambatuasinaga@gmail.com</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-3">
-                <div class="card text-center shadow-sm">
-                    <div class="card-header">
-                        <h5 class="fw-semibold">IPA 3</h5>
-                        <h6>2019</h6>
-                    </div>
-                    <div class="card-body">
-                        <div class="student-img">
-                            <img src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
-                                alt="" class="img-fluid">
-                        </div>
-                        <div class="mt-3">
-                            <h5 class="card-title fw-semibold">Abrahan GT Sinaga</h5>
-                            <p class="card-text">abramtambatuasinaga@gmail.com</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            @endfor
+
+
         </div>
     </div>
 @endsection
