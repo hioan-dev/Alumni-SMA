f81999d7e96f503b5d67a4951e7de1b3f291f0a6
Re-Creators <richardoehbet221@gmail.com>
2023-03-14 21:36:45 +0700

    update berita

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -3,13 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Models\Alumni;
+use App\Models\Berita;
+use App\Models\Kategori;
 use Illuminate\Http\Request;
 
 class FrontendController extends Controller
 {
     public function index()
     {
-        return view('home');
+        $news = Berita::orderBy('created_at', 'DESC')->limit('3')->get();
+        return view('home', compact('news'));
     }
 
     public function alumni()
@@ -20,7 +23,12 @@ class FrontendController extends Controller
 
     public function berita()
     {
-        return view('berita');
+        $news = Berita::paginate(5)->fragment('news');
+        $categories = Kategori::all();
+        return view('berita', [
+            'news' => $news,
+            'categories' => $categories
+        ]);
     }
 
     public function iuran()
--- resources/views/berita.blade.php
+++ resources/views/berita.blade.php
@@ -1,4 +1,6 @@
 @extends('layouts.app')
+@section('title', 'Berita')
+
 
 @push('styles')
     <style>
@@ -43,75 +45,70 @@
         {{ Breadcrumbs::render('berita') }}
 
         <div class="row">
-            <div class="col-md-8">
-                <div class="row gy-3 gx-3 mt-2">
-                    @for ($i = 0; $i < 10; $i++)
-                        <div class="col-md-4">
-                            <div class="card shadow-sm" style="height:400px">
-                                <div class="card-news__img">
-                                    <a href="#">
-                                        <img src="https://images.unsplash.com/photo-1478131143081-80f7f84ca84d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
-                                            alt="">
-                                    </a>
-                                </div>
-                                <div class="card-body">
-                                    <div class="fs-6 mb-2 text-muted d-flex align-content-center">
-                                        <div class="d-flex align-content-center">
-                                            <span class="material-symbols-outlined fs-6 mr-3">
-                                                calendar_month
-                                            </span>
-                                            <div class="px-1" style="font-size:12px;">08 Maret 2021</div>
-                                        </div>
-                                        <div class="d-flex align-content-center">
-                                            <span class="material-symbols-outlined fs-5 mr-3">
-                                                folder
-                                            </span>
-                                            <a href="#" class="px-1 text-decoration-none"
-                                                style="font-size:12px;">Event</a>
-                                        </div>
+
+            @if ($news->count() > 0)
+                <div class="col-md-8">
+                    <div class="row gy-3 gx-3 mt-2">
+                        @foreach ($news as $row)
+                            <div class="col-md-4">
+                                <div class="card shadow-sm" style="height:400px">
+                                    <div class="card-news__img">
+                                        <a href="#">
+                                            <img src="{{ asset('storage/' . $row->banner) }}" class="card-img-top"
+                                                alt="">
+                                        </a>
                                     </div>
-                                    <h5 class="card-title fw-semibold clamp-2">Penyelengaraan Faksinasi Alumni asdasd
-                                        asdasda
-                                        asdasd</h5>
-                                    <p class="card-text clamp-2">Some quick example text to build on the card title and make
-                                        up
-                                        the
-                                        bulk of
-                                        the card's content.</p>
-                                    <a href="#" class="card-link text-decoration-none d-flex align-items-center">Read
-                                        More
-                                        <span class="material-symbols-outlined px-1 fs-5">
-                                            east
-                                        </span></a>
+                                    <div class="card-body">
+                                        <div class="fs-6 mb-2 text-muted d-flex align-content-center">
+                                            <div class="d-flex align-content-center">
+                                                <span class="material-symbols-outlined fs-6 mr-3">
+                                                    calendar_month
+                                                </span>
+                                                <div class="px-1" style="font-size:12px;">
+                                                    {{ date('M j, Y', strtotime($row->created_at)) }}</div>
+                                            </div>
+                                            <div class="d-flex align-content-center">
+                                                <span class="material-symbols-outlined fs-5 mr-3">
+                                                    folder
+                                                </span>
+                                                <a href="#" class="px-1 text-decoration-none"
+                                                    style="font-size:12px;">{{ $row->kategori->nama_kategori }}</a>
+                                            </div>
+                                        </div>
+                                        <h5 class="card-title fw-semibold clamp-2">{{ $row->title }}</h5>
+                                        <p class="card-text clamp-2">{{ $row->description }}</p>
+                                        <a href="#"
+                                            class="card-link text-decoration-none d-flex align-items-center">Read
+                                            More
+                                            <span class="material-symbols-outlined px-1 fs-5">
+                                                east
+                                            </span></a>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    @endfor
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div>
-                    <h5 class="fw-bold">Kategori</h5>
-                    <div class="divider"></div>
-                </div>
-                <div class="row  mt-2 gy-3 gx-2">
-                    <div class="col-auto">
-                        <a href="#" class="category">Event</a>
-                    </div>
-                    <div class="col-auto">
-                        <a href="#" class="category">Technology</a>
-                    </div>
-                    <div class="col-auto">
-                        <a href="#" class="category">Writing</a>
-                    </div>
-                    <div class="col-auto">
-                        <a href="#" class="category">Job Information</a>
-                    </div>
-                    <div class="col-auto">
-                        <a href="#" class="category">Friends</a>
+                        @endforeach
                     </div>
                 </div>
-            </div>
+                <div class="col-md-4">
+                    <div>
+                        <h5 class="fw-bold">Kategori</h5>
+                        <div class="divider"></div>
+                    </div>
+                    <div class="row  mt-2 gy-3 gx-2">
+                        @foreach ($categories as $category)
+                            <div class="col-auto">
+                                <a href="#" class="category">{{ $category->nama_kategori }}</a>
+                            </div>
+                        @endforeach
+                    </div>
+                </div>
+            @else
+                <div class="col-12">
+                    <div class="alert alert-info text-center" role="alert">
+                        Tidak ada berita
+                    </div>
+                </div>
+            @endif
         </div>
     </div>
 @endsection
