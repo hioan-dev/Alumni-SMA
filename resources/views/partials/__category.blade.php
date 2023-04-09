1f97938d08c71979007e3419b4eb8e0a91f87302
unknown <oansmaturapax@gmail.com>
2023-04-08 23:02:59 -0700

    update:category-count

--- resources/views/partials/__category.blade.php
+++ resources/views/partials/__category.blade.php
@@ -6,8 +6,9 @@
     <div class="row  mt-2 gy-3 gx-2">
         @foreach ($categories as $category)
             <div class="col-auto">
-                <a href="{{ route('kategori-berita', $category->slug) }}"
-                    class="category">{{ $category->nama_kategori }}</a>
+                <a href="{{ route('kategori-berita', $category->slug) }}" class="category">{{ $category->nama_kategori }}
+                    <span class="badge bg-primary">{{ $category->berita->count() }}</span></a>
+
             </div>
         @endforeach
     </div>
